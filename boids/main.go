cbe4eadf427afae270ba121c084f1000cf63ea71
willqiang <willqiang32@gmail.com>
2020-10-15 23:24:34 +0800

    BOIDS first version

--- boids/main.go
+++ boids/main.go
@@ -1,1 +1,38 @@
-package boids
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten"
+	"image/color"
+	"log"
+)
+
+const (
+	screenWidth, screenHeight = 640, 360
+	boidCount                 = 500
+)
+
+var (
+	grenn = color.RGBA{10, 255, 50, 255}
+	boids [boidCount]*Boid
+)
+
+func update(screen *ebiten.Image) error {
+	if !ebiten.IsDrawingSkipped() {
+		for _, boid := range boids {
+			screen.Set(int(boid.position.x+1), int(boid.position.y), grenn)
+			screen.Set(int(boid.position.x-1), int(boid.position.y), grenn)
+			screen.Set(int(boid.position.x), int(boid.position.y-1), grenn)
+			screen.Set(int(boid.position.x), int(boid.position.y+1), grenn)
+		}
+	}
+	return nil
+}
+
+func main() {
+	for i := 0; i < boidCount; i++ {
+		createBoid(i)
+	}
+	if err := ebiten.Run(update, screenWidth, screenHeight, 2, "Boids in a box"); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/willqiang/go-multithread
+
+go 1.13
+
+require github.com/hajimehoshi/ebiten v1.12.1
